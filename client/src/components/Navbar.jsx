2d02668644b22e03cd0b30d82abe2ea70331d359
Arpan Mallick <mallickarpan53@gmail.com>
2026-02-05 00:30:10 +0530

    Replace logo image with 'Resume' text in Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
     <div className='shadow bg-white'>
         <div className='flex items-center justify-between max-w-7xl mx-auto px-4 py-3.5 text-slate-800 transition-all'>
             <Link to='/'>
-            <img src="/logo.svg" alt="logo" className='h-11 w-auto'/>
+            <h4 className='h-11 w-auto'>Resume</h4>
             </Link>
             <div className='flex items-center gap-4 text-sm'>
                 <Link to="/app/analyzer" className="hover:text-blue-600 font-medium max-sm:hidden">Resume Analyzer</Link>
